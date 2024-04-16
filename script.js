cee7970ef1fce46f7044f87d183410280088335d
EricCTA <166875244+EricCTA@users.noreply.github.com>
2024-04-16 10:32:10 -0700

    script.js

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 function calculateVolunteers() {
     var voterUniverse = parseInt(document.getElementById("voterUniverse").value);
-    var numHours = parseInt(document.getElementById("numHours").value);
+    var num2HourShifts = parseInt(document.getElementById("num2HourShifts").value);
+    var num4HourShifts = parseInt(document.getElementById("num4HourShifts").value);
 
     var contactRate = 0.15;
     var doorsPerHourPerVolunteer = 15;
     
-    var totalDoorsKnocked = numHours * doorsPerHourPerVolunteer;
-    var expectedContacts = Math.round(totalDoorsKnocked * contactRate);
-    var volunteersNeeded = Math.ceil(voterUniverse / totalDoorsKnocked);
+    var totalDoors2Hour = num2HourShifts * 2 * doorsPerHourPerVolunteer;
+    var totalDoors4Hour = num4HourShifts * 4 * doorsPerHourPerVolunteer;
 
-    document.getElementById("result").innerHTML = "Volunteers Needed: " + volunteersNeeded + "<br>" +
-        "Expected Contacts: " + expectedContacts;
-}
+    var expectedContacts2Hour = Math.round(totalDoors2Hour * contactRate);
+    var expectedContacts4Hour = Math.round(totalDoors4Hour * contactRate);
+
+    var volunteersNeeded2Hour = Math.ceil(voterUniverse / totalDoors2Hour);
+    var volunteersNeeded4Hour = Math.ceil(voterUniverse / totalDoors4Hour);
+
+    document.getElementById("result").innerHTML = "After School Canvassing (2-hour shi
